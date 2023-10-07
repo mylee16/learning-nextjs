4191b0b8d7a0eb513101142fa094a66c796ef720
Mengyong Lee <mengyonglee.ai@gmail.com>
2023-10-07 10:05:38 +0800

    implemented signup and login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,47 @@
 "use client";
 
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import {useRouter} from "next/navigation";
-import {axios} from "axios";
+import axios from "axios";
+import {toast} from "react-hot-toast"; 
 
 export default function LoginPage() {
+    const router = useRouter();
     const [user, setUser] = React.useState({
         email: "",
-        password: "",    })
+        password: "",})
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
 
-    const onLogin = async() => {}
+    const onLogin = async() => {
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/login", user);
+            console.log("Login success", response.data);
+            toast.success("Login success");
 
+            router.push("/profile");
+        }
+
+        catch (error:any) {
+            console.log("Login failed", error.message);
+            toast.error(error.message);
+        }
+        finally {
+            setLoading(false);
+        }
+
+
+    }
+
+    useEffect(() => {
+        if (user.email.length>0 && user.password.length > 0) {
+            setButtonDisabled(false);
+        } else {
+            setButtonDisabled(true);
+        }
+    });
 
     return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
