6cd167c66a66a8ad322e6c7057558bc8947254bf
Mengyong Lee <mengyonglee.ai@gmail.com>
2023-10-08 12:57:55 +0800

    added verify email functionality

---
+++ src/helpers/mailer.ts
@@ -0,0 +1,49 @@
+import nodemailer from 'nodemailer';
+import User from "@/models/userModel";
+import bcrypt from 'bcryptjs';
+
+
+export const sendEmail = async({email, emailType, userId}: any) => {
+    try {
+        const hashedToken = await bcrypt.hash(userId.toString(), 10)
+
+        if (emailType == "VERIFY") {
+            await User.findByIdAndUpdate(
+                userId, 
+                {verifyToken: hashedToken, 
+                verifyTokenExpiry: Date.now() + 3600000})
+        }
+        else if (emailType == "RESET") {
+            await User.findByIdAndUpdate(
+                userId, 
+                {forgotPasswordToken: hashedToken, 
+                forgotPasswordTokenExpiry: Date.now() + 3600000});
+        }
+
+        var transport = nodemailer.createTransport({
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+                user: process.env.EMAILER_USER,
+                pass: process.env.EMAILER_PASS
+            }
+            });
+
+        const mailOptions = {
+            from: 'mengyonglee.ai@gmail.com',
+            to: email,
+            subject: emailType == "VERIFY" ? "Verify your email" : "Reset your password",
+            html: `<p>Click <a href="${process.env.DOMAIN}/${emailType.toLowerCase()}?token=${hashedToken}">here</a> to ${emailType == "VERIFY" ? "verify your email" : "reset your password"}
+            or copy and paste the following link in your browser <br> ${process.env.DOMAIN}/${emailType.toLowerCase()}?token=${hashedToken}
+            </p>`
+        }
+
+        const mailResponse = await transport.sendMail(mailOptions);
+
+        return mailResponse;
+    }
+
+
+    catch (error: any) {
+        throw new Error(error.message);}
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname
-    const isPublicPath = path === "/login" || path === "/signup"
+    const isPublicPath = path === "/login" || path === "/signup" || path === "/verify"
     const token = request.cookies.get("token")?.value || ""
 
     if (isPublicPath && token) {
@@ -21,6 +21,7 @@ export const config = {
         "/",
         "/profile",
         "/login",
-        "/signup"
+        "/signup",
+        "/verify"
     ],
 }
